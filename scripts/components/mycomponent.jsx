3ff96c10b94b33525fc6f0b8d7340146385538f1
Maurizio Mangione <granzebru@gmail.com>
2015-02-20 11:57:54 +0100

    added ES6 arrow function example

--- scripts/components/mycomponent.jsx
+++ scripts/components/mycomponent.jsx
@@ -1,11 +1,23 @@
 'use strict';
 
 var React = require('react'),
+    pack = require('../../package.json'),
 
     Mycomponent = React.createClass({
       render: function() {
+        var version = pack.version,
+            deps;
+
+        deps = Object.keys(pack.devDependencies).map((dep, i) => <li key={i}>{dep}</li>);
+
         return (
-          <h1 className="Mycomponent">Welcome to React Starterify</h1>
+          <div>
+            <h1 className="Mycomponent">Welcome to &#9883; React Starterify {version}</h1>
+            <p>Powered by:</p>
+            <ul>
+              {deps}
+            </ul>
+          </div>
         )
       }
     });
