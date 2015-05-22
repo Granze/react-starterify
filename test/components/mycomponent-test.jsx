cde02ec1005e76ec998afe04c522e69293b4f791
Maurizio Mangione <granzebru@gmail.com>
2015-05-22 17:13:11 +0200

    test scaffold

---
+++ test/components/mycomponent-test.jsx
@@ -0,0 +1,7 @@
+//https://github.com/jesstelford/react-testing-mocha-jsdom/blob/master/test/component/todo-item.js
+
+import React from 'react/addons'
+import assert from 'chai'
+import mycomponent from '../../scripts/components/mycomponent'
+import ReactTestUtils from React.addons.TestUtils
+
