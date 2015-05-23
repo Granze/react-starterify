5e69b466ad3893de14ca7544cf0f1cadfb258cb0
Maurizio Mangione <granzebru@gmail.com>
2015-05-23 18:56:30 +0200

    add some test

--- test/components/mycomponent-test.jsx
+++
@@ -1,7 +0,0 @@
-//https://github.com/jesstelford/react-testing-mocha-jsdom/blob/master/test/component/todo-item.js
-
-import React from 'react/addons'
-import assert from 'chai'
-import mycomponent from '../../scripts/components/mycomponent'
-import ReactTestUtils from React.addons.TestUtils
-
