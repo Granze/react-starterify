0c31f030a32fc6330ecb1897c7fa878527e118f5
Maurizio Mangione <granzebru@gmail.com>
2015-09-05 23:12:36 +0200

    implement react-router

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link }  from 'react-router';
+import packageJSON from '../../package.json';
+
+export default React.createClass({
+  returnSomething(something) {
+    return something;
+  },
+  render() {
+    const version = packageJSON.version;
+
+    return (
+      <div>
+        <header>
+          <h1>React Starterify {version}</h1>
+          <Link to="/about">About</Link>
+          <Link to="/poweredby">Powered by</Link>
+        </header>
+        <section>
+          {this.props.children || 'Welcome to React Starterify'}
+        </section>
+      </div>
+    )
+  }
+});
