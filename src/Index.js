109cc0dfdab723aa695734453ec0f8e55e4cf9e7
Caio Vaccaro <caiovaccaromora@gmail.com>
2015-09-10 12:59:19 -0300

    Added a slash before the "about" route path

--- src/Index.js
+++ src/Index.js
@@ -9,7 +9,7 @@ window.React = React;
 React.render(
   <Router>
     <Route path="/" component={App}>
-      <Route path="about" component={About}/>
+      <Route path="/about" component={About}/>
       <Route path="/poweredby" component={PoweredBy}/>
     </Route>
   </Router>
