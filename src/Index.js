6cd716a2d9e200e9548973982aae38f8dfc67266
Maurizio Mangione <granzebru@gmail.com>
2015-10-12 22:42:29 +0200

    add react DOM

--- src/Index.js
+++ src/Index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { Router, Route } from 'react-router';
 import App from './components/App';
 import PoweredBy from './components/Powered-by';
@@ -6,7 +7,7 @@ import About from './components/About';
 
 window.React = React;
 
-React.render(
+ReactDOM.render(
   <Router>
     <Route path="/" component={App}>
       <Route path="/about" component={About}/>
