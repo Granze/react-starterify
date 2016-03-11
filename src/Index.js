dd8568d41541c16aa8a1b0d047f0455c1344d0ef
Maurizio Mangione <granzebru@gmail.com>
2016-03-11 23:01:17 +0100

    stateless functions + linting

--- src/Index.js
+++ src/Index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {render} from 'react-dom';
-import {Router, Route, hashHistory} from 'react-router';
+import { render } from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 import App from './components/App';
 import PoweredBy from './components/Powered-by';
 import About from './components/About';
@@ -10,8 +10,8 @@ window.React = React;
 render(
   (<Router history={hashHistory}>
     <Route path="/" component={App}>
-      <Route path="/about" component={About}/>
-      <Route path="/poweredby" component={PoweredBy}/>
+      <Route path="/about" component={About} />
+      <Route path="/poweredby" component={PoweredBy} />
     </Route>
   </Router>), document.getElementById('content')
 );
