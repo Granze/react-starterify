f7b48e32846c20ecf3ca66df923a19978d64670a
Maurizio Mangione <granzebru@gmail.com>
2016-03-14 22:44:56 +0100

    refactor

--- test/components/App-test.js
+++ test/components/App-test.js
@@ -13,8 +13,11 @@ test('should have a div as container', t => {
   t.is(app.type, 'div');
 });
 
-test('should have a version number that match the package.json version property', t => {
-  const h1 = app.props.children[0].props.children;
+test('should contains an H1', t => {
+  t.is(app.props.children[0].props.children[0].type, 'h1');
+});
 
-  t.ok(find(h1, <h1>React Starterify {version}</h1>));
+test('should have a version number that match the package.json version property', t => {
+  const h1 = <h1>React Starterify {version}</h1>;
+  t.ok(find(app.props.children[0].props.children[0], h1));
 });
